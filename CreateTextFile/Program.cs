49e957e0c597360b9254ce9f158286b59a21d28e
Vanitha G <131644178+Vanithagk@users.noreply.github.com>
2024-07-27 23:21:14 +0530

    Add files via upload

---
+++ CreateTextFile/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CreateTextFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+                try
+                {
+                    TextWriter tw = new StreamWriter("D:\\CreatedText.txt", true);
+                    tw.WriteLine("New text file is created");
+                    tw.Close();
+                    Console.WriteLine("File Created");
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e);
+                }
+                Console.Read();
+            }
+        }
+    }
+
